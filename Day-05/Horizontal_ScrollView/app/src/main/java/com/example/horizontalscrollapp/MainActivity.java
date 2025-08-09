61f775b0a337a8f5d0b102a0c8ab1dcebb31b08d
Sandyalgo <gadisandeep02@gmail.com>
2025-08-09 23:06:41 +0530

    Added scrollView & Horizontal scrollView Prectice in Day-05

---
+++ Day-05/Horizontal_ScrollView/app/src/main/java/com/example/horizontalscrollapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.horizontalscrollapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Log.e("General","App is Working");
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "This is an Horizontal Scroll", Toast.LENGTH_SHORT).show();
+                Log.d("General","This is an Log");
+            }
+        });
+    }
+}
