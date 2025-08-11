e2a4d0745c5e6f4fd578d6a14c5b3082d5fc90e1
Sandyalgo <gadisandeep02@gmail.com>
2025-08-11 23:44:01 +0530

    Added MultiScreen & Implicit_Intens in Day-07

---
+++ Day-07/MultiScreen_app/app/src/main/java/com/example/multiscreen1/MainActivity2.java
@@ -0,0 +1,32 @@
+package com.example.multiscreen1;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView name;
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent intent = getIntent();
+        String str  = intent.getStringExtra(MainActivity.Extra_Name);
+        name = findViewById(R.id.text2);
+        name.setText("Your Name is " + str);
+    }
+}
